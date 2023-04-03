bbb9672e89ad121334fc83be771105a931d8cb69
mr-gukas <gukasyan.vladimir@mail.ru>
2023-04-03 14:57:52 +0300

    improve readme

---
+++ build/CMakeFiles/frac.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/gukas/asm/MyMandelbrot/main.cpp" "CMakeFiles/frac.dir/main.cpp.o" "gcc" "CMakeFiles/frac.dir/main.cpp.o.d"
+  "/home/gukas/asm/MyMandelbrot/mandelbrot.cpp" "CMakeFiles/frac.dir/mandelbrot.cpp.o" "gcc" "CMakeFiles/frac.dir/mandelbrot.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
